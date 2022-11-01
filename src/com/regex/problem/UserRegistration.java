44d43ffc111c27cd0d19387d783de9971763bf24
sayrish <sayrrish1@gmail.com>
2022-11-01 09:32:01 +0530

    Valid Mobile Number

--- src/com/regex/problem/UserRegistration.java
+++ src/com/regex/problem/UserRegistration.java
@@ -25,13 +25,20 @@ public class UserRegistration {
 			String emailId = "^[a-zA-Z]+([_+-.][a-zA-Z])+[@][a-zA-Z]+[.][a-z]{2,3}([.][a-zA-Z]{2})*$";
 			String inputEmailId = sc.next();
 			
+			System.out.println("Enter Mobile Number");
+			String mobileNumber ="^(91)()[6-9]{1}[0-9]{9}$";
+			String inputmobileNumber = sc.next();
+			
 			UserRegistration ur = new UserRegistration();
 			System.out.println(ur.userName(firstName, inputFirstName));
 			System.out.println(ur.userName(lastName, inputLastName));
-			System.out.println(ur.userName(emailId, inputEmailId));//abc.xyz@bl.co.in
+			System.out.println(ur.userName(emailId, inputEmailId)); //abc.xyz@bl.co.in
+			System.out.println(ur.userName(mobileNumber, inputmobileNumber));
 		}
 
 	}
+
+
 
 
 
