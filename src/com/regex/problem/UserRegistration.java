293c57bdf7a84ee38e6e0ba2b672e377a3cde8d2
sayrish <sayrrish1@gmail.com>
2022-11-01 09:24:24 +0530

    Added: Enter a Valid First Name

--- src/com/regex/problem/UserRegistration.java
+++ src/com/regex/problem/UserRegistration.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	
-	public boolean userName(String regex, String input) {
+	public boolean userName(String regex , String input) {
 		return Pattern.compile(regex).matcher(input).matches();
 	}
 
@@ -13,30 +13,9 @@ public class UserRegistration {
 		try (Scanner sc = new Scanner(System.in)) {
 			System.out.println("Enter First Name :");
 			String firstName = "^[A-Z]{1}[a-z]{2,}$";
-			String inputFirstName = sc.next(); // Kapil = true // kapil = false //
-			
-			System.out.println("Enter Last Name :");
-			String lastName = "^[A-Z]{1}[a-z]{2,}$";
-			String inputLastName = sc.next();
-			
-			System.out.println("Enter Email id :");
-			String emailId = "^[a-zA-Z]+([_+-.][a-zA-Z])+[@][a-zA-Z]+[.][a-z]{2,3}([.][a-zA-Z]{2})*$";
-			String inputEmailId = sc.next();
-			
-			System.out.println("Enter Mobile Number");
-			String mobileNumber ="^(91)()[6-9]{1}[0-9]{9}$";
-			String inputmobileNumber = sc.next(); //7879219494
-			
-			System.out.println("Enter Password");
-			String password = "((?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&*_]).{8,})";
-			String inputPassword = sc.next(); 
-			
+			String input = sc.next(); //Kapil = true // kapil = false //
 			UserRegistration ur = new UserRegistration();
-			System.out.println(ur.userName(firstName, inputFirstName));
-			System.out.println(ur.userName(lastName, inputLastName));
-			System.out.println(ur.userName(emailId, inputEmailId));//abc.xyz@bl.co.in
-			System.out.println(ur.userName(mobileNumber, inputmobileNumber));
-			System.out.println(ur.userName(password, inputPassword));
+			System.out.println(ur.userName(firstName, input));
 		}
 
 	}
